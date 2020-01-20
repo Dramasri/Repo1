989960fa8039581c1f02d9d0bfc38551348204fd
Dramasri <dramaapr01@gmail.com>
2020-01-20 15:11:13 +0530

    code in local repository

---
+++ src/org/repo/Ropose.java
@@ -0,0 +1,28 @@
+package org.repo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Ropose {
+	
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\java and eclipse 32 bit\\eclipse\\MavenPrjt\\MavenAdac\\ChromeDriver\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("www.facebook.com");
+		
+		driver.getTitle();
+		
+		String name = driver.getCurrentUrl();
+		
+		System.out.println(name);
+		
+		driver.quit();
+		
+		
+		
+	}
+
+}
